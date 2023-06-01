688f2ec113e56924b5facf95ae7074d3e3890963
Mateus Brandão <mateusfleria@gmail.com>
2023-06-01 09:06:38 -0300

    Movendo tipagens do portal para shared-components

--- types/index.tsx
+++ types/index.tsx
@@ -1,3 +1,51 @@
+export interface User{
+  id: string;
+  email: string,
+  picture: string,
+  active?: boolean,
+  name: string,
+  clients?: Client[],
+  permitions?: Permition[],
+  permitions_slug?: PossiblePermissions[],
+  flow_permission?: string,
+  flow_actions_permitted?: string[],
+  client_name?: string,
+  token: string,
+  current_client: string,
+  userCategories?: UserCategory[],
+  userCategory?: UserCategory
+}
+export interface Client{
+  id: string,
+  cnpj: string,
+  razao_social: string,
+  nome_fantasia: string,
+  picture: string,
+
+  cep: string,
+  logradouro: string,
+  numero: string,
+  bairro: string,
+  cidade: string,
+  estado: string,
+  
+  ddd: string,
+  telefone: string,
+  email: string,
+
+  users?: User[],
+
+  active?: boolean
+}
+export interface UserCategory{
+  id: string,
+  slug: string,
+  name: string,
+  description: string,
+  clientId: string,
+  permitions: Permition[] 
+}
+//#region PERIMTIONS
 export enum PossiblePermissions {
   APPROVAL = 'aprovacao',
   ADMIN =  'admin',
@@ -9,3 +57,14 @@ export enum PossiblePermissions {
   PLANILHA = 'sheet',
   DASH= 'dash'
 }
+export interface PermissionType{
+  name: string,
+  actions: string[]
+}
+export interface Permition{
+  id: string,
+  name: string,
+  slug?: string,
+  description?: string,
+}
+//#endregion PERIMTIONS
