4273050400517bf6956c9a343236a5f160afb7b7
Mateus Brandão <mateusfleria@gmail.com>
2023-07-18 22:51:16 -0300

    feat: Lidando com permissões do menu de forma visual

--- MenuSlider/index.tsx
+++ MenuSlider/index.tsx
@@ -17,7 +17,7 @@ import { useNotify } from "../../contexts/NotifyContext";
 import { useNavigate } from "react-router-dom";
 import { DropdownChooseEnterprise } from "../Wrapper/v3/DropdownChooseEnterprise";
 import { ButtonHelp } from "../Wrapper/v3/ButtonHelp";
-import { EnvelopeIcon, FlowIcon } from "../utils/icons";
+import { EnvelopeIcon, FlowIcon, LockIcon, RefreshIcon } from "../utils/icons";
 import { useEffect, useState } from "react";
 import { getPublishedFlows } from "../services/workflow";
 import { getUrls } from "../services/conn/api";
@@ -116,7 +116,7 @@ export const MenuSlider = () => {
 
       <div className="flex-col sm:flex-row flex-wrap flex sm:justify-between px-6 lg:max-w-[95%] xl:max-w-[1580px] w-full mx-auto gap-6">
         <section className="flex bg-card-gray flex-1 min-w-[375px] h-[460px] rounded-2xl">
-          <div className="flex flex-wrap mt-12 ml-6 mr-5 mb-80 gap-8">
+          <div className="flex flex-wrap my-12 mx-6 gap-8 w-full content-start">
             {workflows.map((flow) => (
               <div className="flex flex-col w-[65px]" key={flow._id}>
                 <button className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg mr-1 hover:bg-primary-600 hover:scale-105" style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
@@ -129,7 +129,7 @@ export const MenuSlider = () => {
               </div>
             ))}
 
-            {user && user.current_client && Object.keys(clientsWithAccessToCAP).includes(user.current_client) && portalItem && (
+            {(user && user.current_client && Object.keys(clientsWithAccessToCAP).includes(user.current_client) && portalItem) ? (
               <div className="flex flex-col w-[65px]">
                 <button
                   className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg mr-1 hover:bg-primary-600 hover:scale-105"
@@ -140,109 +140,109 @@ export const MenuSlider = () => {
                 </button>
                 <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">Contas a pagar</span>
               </div>
-            )}
+            ) : workflows.length === 0 ? (
+              <p className="
+                w-full h-full
+                flex items-center justify-center
+                text-center text-sm text-gray-500
+              ">Você não possui nenhum aplicativo criado</p>
+            ) : <></>}
           </div>
         </section>
 
         <section className="flex bg-card-gray flex-1 min-w-[375px] h-[460px] rounded-2xl">
-          <div className="flex flex-wrap mt-12 ml-6 mr-5 mb-80 gap-8">
-
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-600 hover:scale-105 flex items-center justify-center text-white"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => redirectToApp({
+          <div className="flex flex-wrap my-12 mx-6 gap-8 w-full content-start">
+            {[
+              { 
+                redirect: {
                   url: `${frontURL.wf}?token=${user?.token}`,
                   disabled: !(user && user.permitions_slug && user.permitions_slug.includes(PossiblePermissions.ISAC))
-                }, toast, navigate)}
-              >
-                <FlowIcon w={34} h={34}/>
-              </button>
-              <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">ISAC</span>
-            </div>
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-600 hover:scale-105 flex items-center justify-center text-white"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => redirectToApp({
+                },
+                icon: <FlowIcon w={34} h={34}/>, name: 'ISAC'
+              },{
+                redirect: {
                   url: `${frontURL.wf}modelos/?token=${user?.token}`,
                   disabled: !(user && user.permitions_slug && user.permitions_slug.includes(PossiblePermissions.ISAC))
-                }, toast, navigate)}
-              >
-                <EnvelopeIcon w={34} h={34}/>
-              </button>
-              <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">Modelo de Mensagens</span>
-            </div>
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-600 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => toast.warning("Em desenvolvimento!")}
-              >
-                <img src={view} alt="wallet icon" className="mx-auto pt-1" />
-              </button>
-              <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">Vision 360</span>
-            </div>
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-600 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => redirectToApp({
+                },
+                icon: <EnvelopeIcon w={34} h={34}/>, name: 'Modelo de Mensagens'
+              },{ icon: <img src={view} alt="wallet icon" className="mx-auto pt-1" />, name: 'Vision 360' },{
+                redirect: {
                   url: `${frontURL.portal}/co-pilot-dashboard`,
                   disabled: !(user && user.permitions_slug && user.permitions_slug.includes(PossiblePermissions.DASH))
-                }, toast, navigate)}
-              >
-                <img src={leaderboard} alt="coin icon" className="mx-auto" />
-              </button>
-              <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">Dashboard</span>
-            </div>
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-600 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => toast.warning("Em desenvolvimento!")}
-              >
-                <img src={addFolder} alt="add folder icon" className="mx-auto"/>
-              </button>
-              <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">Reports</span>
-            </div>
+                },
+                icon: <img src={leaderboard} alt="coin icon" className="mx-auto" />, name: 'Dashboard'
+              },{ icon: <img src={addFolder} alt="add folder icon" className="mx-auto"/>, name: 'Reports' }
+            ].map((item) => (
+              <div className="flex flex-col w-[65px]" key={item.name}>
+                <button
+                  className="
+                    bg-primary-500 w-[65px] h-[65px] shrink-0 rounded-lg
+                    hover:bg-primary-600 hover:scale-105 text-white
+                    flex items-center justify-center relative
+                    overflow-hidden
+                  "
+                  style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+                  onClick={() => {
+                    if(item.redirect) redirectToApp(item.redirect, toast, navigate)
+                    else toast.warning("Em desenvolvimento!")
+                  }}
+                >
+                  {item.icon}
+                  {(!item.redirect || item.redirect.disabled) && (
+                    <span className="bg-gray-800/40 absolute inset-0 flex items-center justify-center">
+                      {item.redirect ? <LockIcon/> : <RefreshIcon/> }
+                    </span>
+                  )}
+                </button>
+                <span className="text-primary-500 text-xs text-center truncate hover:whitespace-normal mt-3">{item.name}</span>
+              </div>
+            ))}
           </div>
         </section>
 
         <section className="flex bg-card-gray flex-1 min-w-[375px] h-[460px] rounded-2xl">
-          <div className="flex flex-wrap mt-12 ml-6 mr-5 mb-80 gap-8">
-
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-200 w-[65px] h-[65px] shrink-0 rounded-lg mr-12 hover:bg-primary-100 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => redirectToApp({ url: `${frontURL.portal}/perfil` }, toast, navigate)}>
-                <img src={profileCircle} alt="wallet icon" className="mx-auto" />
-              </button>
-              <span className="text-primary-200 text-xs text-center truncate hover:whitespace-normal mt-3">Perfil</span>
-            </div>
-
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-200 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-100 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => toast.warning("Em desenvolvimento!")}
-              >
-                <img src={multiplePages} alt="coin icon" className="mx-auto" />
-              </button>
-              <span className="text-primary-200 text-xs text-center truncate hover:whitespace-normal mt-3">Meus Doc.</span>
-            </div>
-
-            <div className="flex flex-col w-[65px]">
-              <button
-                className="bg-primary-200 w-[65px] h-[65px] shrink-0 rounded-lg hover:bg-primary-100 hover:scale-105"
-                style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-                onClick={() => redirectToApp({ url: `${frontURL.portal}/painel-adm`, disabled: !user?.permitions_slug?.includes(PossiblePermissions.ADMIN) }, toast, navigate)}
-              >
-                <img src={settings} alt="home sale icon" className="mx-auto"/>
-              </button>
-              <span className="text-primary-200 text-xs text-center truncate hover:whitespace-normal mt-3">Adm Center</span>
-            </div>
+          <div className="flex flex-wrap my-12 mx-6 gap-8 w-full content-start">
+            {[
+              {
+                redirect: { url: `${frontURL.portal}/perfil` },
+                icon: <img src={profileCircle} alt="wallet icon" className="mx-auto" />,
+                name: 'Perfil'
+              },{
+                icon: <img src={multiplePages} alt="coin icon" className="mx-auto" />,
+                name: 'Meus Docs.'
+              },{
+                redirect: {
+                  url: `${frontURL.portal}/painel-adm`,
+                  disabled: !user?.permitions_slug?.includes(PossiblePermissions.ADMIN)
+                },
+                icon: <img src={settings} alt="home sale icon" className="mx-auto"/>,
+                name: 'Adm Center'
+              }
+            ].map((item) => (
+              <div className="flex flex-col w-[65px]" key={item.name}>
+                <button
+                  className="
+                    bg-primary-200 w-[65px] h-[65px] shrink-0 rounded-lg
+                    flex items-center justify-center relative
+                    hover:bg-primary-100 hover:scale-105
+                    overflow-hidden
+                  "
+                  style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+                  onClick={() => {
+                    if(item.redirect) redirectToApp(item.redirect, toast, navigate)
+                    else toast.warning("Em desenvolvimento!")
+                  }}
+                >
+                  {item.icon}
+                  {(!item.redirect || item.redirect.disabled) && (
+                    <span className="bg-gray-800/40 absolute inset-0 flex items-center justify-center text-white">
+                      {item.redirect ? <LockIcon/> : <RefreshIcon/> }
+                    </span>
+                  )}
+                </button>
+                <span className="text-primary-200 text-xs text-center truncate hover:whitespace-normal mt-3">{item.name}</span>
+              </div>
+            ))}
           </div>
         </section>
         <div className="absolute bottom-8 right-4 flex flex-col justify-between z-50">
