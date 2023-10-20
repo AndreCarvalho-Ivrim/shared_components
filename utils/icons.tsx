6a65306c534b2ca038605e4757ef281748300365
Mateus <mateusfleria@gmail.com>
2023-10-20 11:33:02 -0300

    feat:

--- utils/icons.tsx
+++ utils/icons.tsx
@@ -511,13 +511,23 @@ export const WindowsIcon = (props: IconProps) => (
 export const GitIcon = (props: IconProps) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg"
-    width={props.w ?? "24"} height={props.h ?? "24"}
+    viewBox="0 0 24 24"
     className={props.className ?? ''}
-    viewBox="0 0 24 24" style={{
-      fill: props.color ?? 'currentColor'
-    }}
+    width={props.w ?? "24"} height={props.h ?? "24"}
+    style={{ fill: props.color ?? 'currentColor' }}
   >
     <path d="M5.559 8.855c.166 1.183.789 3.207 3.087 4.079C11 13.829 11 14.534 11 15v.163c-1.44.434-2.5 1.757-2.5 3.337 0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5c0-1.58-1.06-2.903-2.5-3.337V15c0-.466 0-1.171 2.354-2.065 2.298-.872 2.921-2.896 3.087-4.079C19.912 8.441 21 7.102 21 5.5 21 3.57 19.43 2 17.5 2S14 3.57 14 5.5c0 1.552 1.022 2.855 2.424 3.313-.146.735-.565 1.791-1.778 2.252-1.192.452-2.053.953-2.646 1.536-.593-.583-1.453-1.084-2.646-1.536-1.213-.461-1.633-1.517-1.778-2.252C8.978 8.355 10 7.052 10 5.5 10 3.57 8.43 2 6.5 2S3 3.57 3 5.5c0 1.602 1.088 2.941 2.559 3.355zM17.5 4c.827 0 1.5.673 1.5 1.5S18.327 7 17.5 7 16 6.327 16 5.5 16.673 4 17.5 4zm-4 14.5c0 .827-.673 1.5-1.5 1.5s-1.5-.673-1.5-1.5.673-1.5 1.5-1.5 1.5.673 1.5 1.5zM6.5 4C7.327 4 8 4.673 8 5.5S7.327 7 6.5 7 5 6.327 5 5.5 5.673 4 6.5 4z"></path>
+  </svg>
+)
+export const GitCompareIcon = (props: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={props.className ?? ''}
+    width={props.w ?? "24"} height={props.h ?? "24"}
+    style={{ fill: props.color ?? 'currentColor' }}
+  >
+    <path d="M6.01 2c-1.93 0-3.5 1.57-3.5 3.5 0 1.58 1.06 2.903 2.5 3.337v7.16c-.001.179.027 1.781 1.174 2.931C6.892 19.64 7.84 20 9 20v2l4-3-4-3v2c-1.823 0-1.984-1.534-1.99-2V8.837c1.44-.434 2.5-1.757 2.5-3.337 0-1.93-1.571-3.5-3.5-3.5zm0 5c-.827 0-1.5-.673-1.5-1.5S5.183 4 6.01 4s1.5.673 1.5 1.5S6.837 7 6.01 7zm13 8.163V7.997C19.005 6.391 17.933 4 15 4V2l-4 3 4 3V6c1.829 0 2.001 1.539 2.01 2v7.163c-1.44.434-2.5 1.757-2.5 3.337 0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5c0-1.58-1.06-2.903-2.5-3.337zm-1 4.837c-.827 0-1.5-.673-1.5-1.5s.673-1.5 1.5-1.5 1.5.673 1.5 1.5-.673 1.5-1.5 1.5z"></path>
   </svg>
 )
 export const WidgetIcon = (props: IconProps) => (
@@ -1026,6 +1036,7 @@ export const listAvailableIcons : { component: (props?: IconProps) => ReactNode,
   {component: (props?: IconProps) => <MoreVerticalIcon {...props}/>,    title: 'MoreVerticalIcon'     },
   {component: (props?: IconProps) => <WindowsIcon {...props}/>,         title: 'WindowsIcon'          },
   {component: (props?: IconProps) => <GitIcon {...props}/>,             title: 'GitIcon'              },
+  {component: (props?: IconProps) => <GitCompareIcon {...props}/>,      title: 'GitCompareIcon'       },
   {component: (props?: IconProps) => <WidgetIcon {...props}/>,          title: 'WidgetIcon'           },
   {component: (props?: IconProps) => <FormIcon {...props}/>,            title: 'FormIcon'             },
   {component: (props?: IconProps) => <DetalistIcon {...props}/>,        title: 'DetalistIcon'         },
@@ -1066,7 +1077,7 @@ export const listAvailableIcons : { component: (props?: IconProps) => ReactNode,
   {component: (props?: IconProps) => <TripIcon {...props}/>,            title: 'TripIcon'             },
   {component: (props?: IconProps) => <ConnectionIcon {...props}/>,      title: 'ConnectionIcon'       },
   {component: (props?: IconProps) => <CarouselIcon {...props}/>,        title: 'CarouselIcon'         },
-  {component: (props?: IconProps) => <TimeIcon {...props}/>,            title: 'TimeIcon'         },
+  {component: (props?: IconProps) => <TimeIcon {...props}/>,            title: 'TimeIcon'             },
 ];
 export const getIconByName = (title: AvailableIcons, props?: IconProps) : ReactNode => {
   const findedIcon = listAvailableIcons.find(icon => icon.title === title);
