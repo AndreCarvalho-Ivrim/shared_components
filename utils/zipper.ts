cae8152ad2af51b620b16334cb076fac24d97726
Mateus <mateusfleria@gmail.com>
2023-10-27 01:02:25 -0300

    feat:

---
+++ utils/zipper.ts
@@ -0,0 +1,26 @@
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
+export const downloadZip = async (files: { url: string, name: string }[], zipName?: string) => {
+  const zip = new JSZip();
+  const zipFilename = zipName ? `${zipName}.zip` : "download.zip";
+
+  const addFileToZip = async (url: string, filename: string) => {
+    // [ ] add tratamento de erro
+    // [ ] add progress control
+    const response = await fetch(url);
+    const blob = await response.blob();
+
+    const sanitizedFilename = filename.replace(/[^\w\d-_.]/gi, '_');
+
+    zip.file(sanitizedFilename, blob);
+  };
+
+  await Promise.all(
+    files.map((file) => addFileToZip(file.url, file.name))
+  );
+
+  zip.generateAsync({ type: "blob" }).then((content) => {
+    saveAs(content, zipFilename);
+  });
+};
