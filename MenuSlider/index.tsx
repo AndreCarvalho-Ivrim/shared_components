594c080772da801ca338e08d8ef6f012607f1241
Mateus Brandão <mateusfleria@gmail.com>
2024-09-10 14:58:34 -0300

    feat: Substituindo ternário por fn disabled

--- MenuSlider/index.tsx
+++ MenuSlider/index.tsx
@@ -350,11 +350,10 @@ export const MenuSlider = () => {
             <button
               type="button"
               className="bg-primary-600 xsm:col-span-2 m-1 h-26 rounded-md flex flex-col justify-center items-center relative"
-              onClick={
-                user && user.permitions_slug && user.permitions_slug.includes(PossiblePermissions.FINANCIAL_CLOSINGS)
-                  ? () => redirectToApp({ url: handleRegexUrl('@hub:closing_folder.home', user?.token) }, toast, navigate)
-                  : undefined
-              }
+              onClick={() => redirectToApp({
+                url: handleRegexUrl('@hub:closing_folder.home', user?.token),
+                disabled: !(user?.permitions_slug && user.permitions_slug.includes(PossiblePermissions.FINANCIAL_CLOSINGS))
+              }, toast, navigate)}
             >
               <img src={FolderFinance} alt="Icone de arquivos" width={50} height={100} className="mt-3" />
               <span className="text-xs text-white pb-1 pt-2 w-full truncate hover:whitespace-normal font-semibold">Fechamentos Financeiros</span>
