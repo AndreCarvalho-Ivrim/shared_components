32a5c0c12a73a6a7cd1791f23eba8ce077d8a03f
Franciel Santos Silva <francielrpm@gmail.com>
2025-03-17 12:02:32 +0000

    Alteração no buttonHelp para chamados

--- Wrapper/v3/ButtonHelp.tsx
+++ Wrapper/v3/ButtonHelp.tsx
@@ -1,28 +1,38 @@
 import { Textarea } from "flowbite-react";
 import { useNotify } from "../../../contexts/NotifyContext";
 import { ModalOptionsType } from "../../utils/Modal";
+import { CreatePublicPostDataBody, requestPublicPost } from "../../../services/publicRoutes";
 
 export const handleHelp = (
   showMessage: (content: React.ReactNode, options: ModalOptionsType) => void,
-  onCloseModal: () => void
+  onCloseModal: () => void,
+  toast: any
 ) => showMessage((
   <div>
     <p className="text-sm text-gray-600 mb-3">
-      Envie um email para nossa equipe de suporte que logo entraremos em contato.
+      Criar Chamado
     </p>
+
+    <label htmlFor="modal-help-textarea" className="text-sm font-medium text-gray-700 block mb-1">
+      Descrição do problema
+    </label>
     <Textarea
       placeholder="Digite sua mensagem"
       required={true}
-      className="mb-3"
+      className="mb-4"
       rows={4}
       id="modal-help-textarea"
     />
-    <p className="text-sm text-gray-600">
-      Para nos ajudar a resolver:<br/>
-      - insira o link da pagina onde o problema foi identificado<br/>
-      - informe o numero da PO e seu usuário<br/>
-      - descreva de forma simples e objetiva o problema encontrado
+
+    <p className="text-sm text-gray-600 mb-4">
+      Para nos ajudar a resolver:
+      <br />- descreva de forma simples e objetiva o problema encontrado
     </p>
+
+    <label htmlFor="file-upload" className="text-sm font-medium text-gray-700 block mb-1">
+      Anexar arquivos (opcional)
+    </label>
+
   </div>
 ), {
   title: 'Precisa de ajuda?',
@@ -30,23 +40,39 @@ export const handleHelp = (
     theme: 'primary',
     text: 'Enviar',
     autoClose: false,
-    onClick: () => {
+    onClick: async () => {
       let el = document.getElementById('modal-help-textarea') as HTMLTextAreaElement;
-      let body = '';
-
-      if(el?.value){
-        body = encodeURIComponent(el.value);
-        el.value = '';
+      let body: CreatePublicPostDataBody = {
+        data: {
+          attachments: []
+        }
       }
+
+      if (el?.value) {
+        console.log('val', el?.value);
+        
+        body.data['description'] = el.value;
+      }
+
+      try {
+        await requestPublicPost({
+          flow_id: '67cb372297f9c94d7e7e73bd',
+          variation: 'registration',
+          fields: body
+        });
+        toast.success("Chamado enviado com sucesso");
+      } catch (e) {
+        toast.error("Não foi possível enviar o chamado");
+      }
+
       
-      window.location.href = `mailto:suportesistemas@ivrim.com.br?subject=${encodeURIComponent('Suporte Portal')}&body=${body}`;
       onCloseModal();
     }
   }
 });
 
 export const ButtonHelp = () => {
-  const { showMessage, onCloseModal } = useNotify();
+  const { showMessage, onCloseModal, toast } = useNotify();
   return (
     <button
       type="button"
@@ -55,7 +81,7 @@ export const ButtonHelp = () => {
         w-12 h-12 flex items-center justify-center rounded-full
         self-end font-semibold text-xl
       `}
-      onClick={() => handleHelp(showMessage, onCloseModal)}
+      onClick={() => handleHelp(showMessage, onCloseModal, toast)}
     >?</button>
   );
 }
