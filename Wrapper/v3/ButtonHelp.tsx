b34243094a11a209f70698b19202f99c69be89d1
Mateus Brandão <mateusfleria@gmail.com>
2025-03-19 13:42:24 -0300

    feat: Melhorando buttonHelp

--- Wrapper/v3/ButtonHelp.tsx
+++ Wrapper/v3/ButtonHelp.tsx
@@ -9,12 +9,11 @@ import { useAuth } from "../../../contexts/AuthContext";
 import { FileList } from "./FileList";
 import { User, WorkflowConfigFilterRefType, WorkflowConfigFilterType } from "../../../shared-types";
 import { Modal } from "../../utils/Modal";
-import { Dropdown } from "../../utils/Dropdown";
-import { TableFooter } from "../../TableFooter";
-import { ArrowRightIcon, EnvelopeIcon, EnvelopeOpenIcon, DropboxIcon, ArchiveIcon } from "../../utils/icons";
+import { EnvelopeIcon, EnvelopeOpenIcon, DropboxIcon, ArchiveIcon, ChevronDownIcon } from "../../utils/icons";
 import moment from "moment";
 import { getDomain, getSupportKeys } from "../../../shared-types/utils/routes";
 import { CreatePublicPostDataBody, requestPublicPost } from "../../services/publicRoutes";
+import { toast } from "react-toastify";
 
 const supportKeys = getSupportKeys();
 const hardcodeSupport = {
@@ -25,7 +24,22 @@ const hardcodeSupport = {
     "in-progress":         supportKeys.steps["in-progress"]         || "67da4442d458ede945918d13",
     "internal-test":       supportKeys.steps["internal-test"]       || "67da4442d458ede945918d14",
     "approval-test":       supportKeys.steps["approval-test"]       || "67da4442d458ede945918d15",
-    "called-closed":       supportKeys.steps["called-closed"]       || "67da4442d458ede945918d16"
+    "called-closed":       supportKeys.steps["called-closed"]       || "67da4442d458ede945918d16"  
+  }
+}
+
+export interface CalledType{
+  _id: string,
+  current_step_id: string,
+  created_at: string,
+  updated_at: string,
+  data: {
+    user: { id: string, name: string, email: string },
+    client: { id: string, name: string },
+    description: string,
+    attachments?: { id: string, name: string, url: string }[],
+    internal_review_description?: string,
+    created_call_url: string,
   }
 }
 
@@ -54,93 +68,14 @@ interface FlowDataRequestFilter{
   pagination?: any
 }
 
-type CallStatusType = 'open' | 'in_progress' | 'finished'
-
 export const ButtonHelp = () => {
   const [files, setFiles] = useState<FileListType[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [viewMode, setViewMode] = useState<'inital' | 'create' | 'list'>('inital');
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [filter, setFilter] = useState<{ status: CallStatusType }>({ status: 'open' });
-  const [totalPartial, setTotalPartial] = useState<Record<string, number>>({})
-  const [total, setTotal] = useState<Record<CallStatusType, number | undefined>>({
-    open: undefined,
-    in_progress: undefined,
-    finished: undefined
-  });
-
-  const [showingCalled, setShowingCalled] = useState<any[]>([]);
-  const [openCalls, setOpenCalls] = useState<any[]>([]);
-  const [callsInProgress, setCallsInProgress] = useState<any[]>([]);
-  const [completedCalls, setCompletedCalls] = useState<any[]>([]);
-
-  const [pageIndex, setPageIndex] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const perPage = 10;
-
   const { toast } = useNotify();
   const { user, client } = useAuth();
 
-  useEffect(() => { onLoad() },[user])
-  useEffect(() => {
-    setShowingCalled(handleFilter())
-  },[openCalls, callsInProgress, completedCalls, filter]);
-
-  async function onLoad(){
-    if(!user || isLoading) return;
-
-    let targetNotifications : any[] = filter.status === 'open' ? openCalls :
-      filter.status === 'in_progress' ? callsInProgress :
-      filter.status === 'finished' ? completedCalls : []
-    const dispatcher = filter.status === 'open' ? setOpenCalls :
-      filter.status === 'in_progress' ? setCallsInProgress :
-      filter.status === 'finished' ? setCompletedCalls : undefined
-
-    setIsLoading(true);
-    await (async () => {
-      const filters: FlowDataRequestFilter = {
-        excludeIds: targetNotifications.map((n: any) => n._id),
-        query: [
-          { ref: '@user.email', type: 'list', value: user.email}
-        ]
-      };
-
-      if (filter.status === 'open') {
-        filters.query?.push({ ref: '@step_id', type: 'select', value: ['67cb372297f9c94d7e7e73be'] });
-      } else if (filter.status === 'in_progress') {
-        filters.query?.push({ ref: '@step_id', type: 'select', value: ['67cb372297f9c94d7e7e73be'] })
-      } else if (filter.status === 'finished') {
-        filters.query?.push({ ref: '@step_id', type: 'select', value: ['67cee063f760d12bf7379d12'] })
-      }
-
-      const res: any = await wf.post(`/flow-data/get/${hardcodeSupport.flow_id}`, filters, { ...(headerBearer(user!.token)) });
-      console.log('RES', res);
-      
-      if(!res.result){
-        toast.error(res.response)
-        return;
-      }
-
-      if(!res.data) return;
-      const { data } = res;
-      console.log('dispatcher', dispatcher);
-      if(dispatcher){
-        const ids = data.datas.map((d: any) => d._id)
-        dispatcher((prevState) => [
-          ...prevState.filter(state => !ids.includes(state._id)),
-          ...res.data!.datas
-        ])
-      }
-      if(res.data.total > -1){
-        setTotalPartial((prevState) => ({
-          ...prevState,
-          [`${filter.status}`]: res.data!.total
-        }))
-      }
-    })()
-    setIsLoading(false)
-  }
   async function handleSubmit() {
     let el = document.getElementById('modal-help-textarea') as HTMLTextAreaElement;
     let description = el?.value;
@@ -182,21 +117,6 @@ export const ButtonHelp = () => {
     }
     
     setIsOpen(false);
-  }
-  function handleFilter() : any[] {
-    let targetNotifications : any[] = filter.status === 'open' ? openCalls :
-      filter.status === 'in_progress' ? callsInProgress :
-      filter.status === 'finished' ? completedCalls : []
-
-    if(targetNotifications.length < perPage){
-      if(total[filter.status] === undefined || total[filter.status]! >= perPage){
-        console.log('[handle-filter:on-load-called]')
-        onLoad()
-      }
-    }
-    console.log('targetNotifications', targetNotifications);
-    
-    return targetNotifications;
   }
   
   return (
@@ -240,16 +160,7 @@ export const ButtonHelp = () => {
             user={user}
           />
         ): viewMode === 'list' ? (
-          <ListCallsContent
-            isLoading={isLoading}
-            filter={filter}
-            setFilter={setFilter}
-            showingNotifications={showingCalled}
-            pageIndex={pageIndex}
-            perPage={perPage}
-            total={total}
-            totalPages={totalPages}
-          />
+          <ListCallsContent />
         ):(
           <div className="flex justify-center items-center mt-6 mb-2">
             <div className="flex gap-6 flex-wrap justify-center">
@@ -428,236 +339,94 @@ const CreateCallFormContent = ({
   )
 }
 
-const ListCallsContent = ({ isLoading, filter, setFilter, showingNotifications, pageIndex, perPage, total, totalPages }: {
-  isLoading: boolean,
-  filter: { status: CallStatusType },
-  setFilter: React.Dispatch<React.SetStateAction<{ type?: string, status: CallStatusType }>>,
-  showingNotifications: any[],
-  pageIndex: number,
-  perPage: number,
-  total: Record<CallStatusType, number | undefined>,
-  totalPages: number
-}) => {
-  console.log('show', showingNotifications);
-  const [hasSelected, setHasSelected] = useState(false);
+const ListCallsContent = () => {
+  const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [calleds, setCalleds] = useState<CalledType[]>([]);
   
-  const [isFiltering, setIsFiltering] = useState(false);
-  const [canPreviousPage, setCanPreviousPage] = useState(false);
-  const [canNextPage, setCanNextPage] = useState(false);
+  useEffect(() => { onLoad() },[user])
 
-  const callStatus : Record<CallStatusType, string> = {
-    open: 'Em Aberto',
-    in_progress: 'Em Andamento',
-    finished: 'Finalizados',
+  async function onLoad(){
+    if(!user || isLoading) return;
+
+    setIsLoading(true);
+    await (async () => {
+      const filters: FlowDataRequestFilter = {
+        excludeIds: [],
+        query: [
+          { ref: 'user.id', type: 'select', value: user.id },
+          { ref: 'client.id', type: 'select', value: user.current_client },
+          { ref: '@step_id', type: 'not', value: hardcodeSupport.steps['called-closed'] }
+        ]
+      };
+
+      const res: any = await wf.post(`/flow-data/get/${hardcodeSupport.flow_id}`, filters, { ...(headerBearer(user!.token)) });
+      
+      if(!res.result){
+        toast.error(res.response)
+        return;
+      }
+
+      if(!res.data) return;
+      const { data } = res;
+
+      setCalleds(data);
+    })()
+    setIsLoading(false)
+  }
+
+  function handleAccessCalled(_id: string){
+
   }
 
   return (
     <div className="mb-6">
-      <div className="bg-gradient-light backdrop-blur-[25px] rounded-lg flex justify-between gap-2 mt-4 mb-4 px-4 text-primary-700 relative z-10">
-        <div className="md:hidden max-sm:w-full">
-          <Dropdown
-            classNames={{ list: `
-              absolute right-0 z-10
-              -mt-1 w-56 origin-top-right rounded-md
-              bg-gray-100/40 backdrop-blur-[25px] shadow-lg
-              ring-1 ring-black ring-opacity-5
-              focus:outline-none pb-2
-            `, button: 'max-sm:w-full', wrapper: 'relative inline-block text-left group max-sm:w-full' }}
-            trigger={
-              <div
-                className="my-3 w-40 max-sm:w-full h-10 border border-gray-50/10 flex items-center justify-between p-4 rounded-md bg-gray-100/5 text-sm backdrop-blur-[25px] font-semibold"
-              >
-                
-                {callStatus[filter.status] ?? 'Status de Notificação'}
-                <ArrowRightIcon className="rotate-90 group-data-[headlessui-state=open]:-rotate-90"/>
-              </div>
-            }
-          >
-            {Object.entries(callStatus).map(([status, name]) => (
-              <button
-                className="text-gray-700 font-semibold text-sm rounded-lg backdrop-blur-[25px] w-full text-start p-2 hover:bg-gray-50/30"
-                onClick={() => setFilter((prevState) => ({
-                  ...prevState,
-                  status: status as CallStatusType
-                }))}
-                key={status}
-              >{name}</button>
-            ))}
-          </Dropdown>
-        </div>
-        <ul className="hidden md:flex gap-4 text-sm h-16 text-primary-700">
-          {Object.entries(callStatus).map(([status, name]) => (
-            <li className={`group ${filter.status === status ? 'active':''}`} key={status}>
-              <button
-                type="button"
-                className={`
-                  relative h-full px-2 text-center font-semibold
-                  
-                  after:w-full after:h-1
-                  after:-bottom-0.5 after:left-0
-                  after:rounded-lg
-
-                  group-[.active]:after:content['']
-                  group-[.active]:after:absolute 
-                  group-[.active]:after:block 
-                  group-[.active]:after:bg-[#6DBFFF] group-[.active]:after:shadow-[0_1px_20px_#6DBFFF]
-
-                  hover:after:content['']
-                  hover:after:absolute 
-                  hover:after:block 
-                  hover:after:bg-[#6DBFFF30]
-                  hover:text-primary-500/80
-                `}
-                onClick={() => setFilter((prevState) => ({
-                  ...prevState,
-                  status: status as CallStatusType
-                }))}
-              >{name}</button>
-            </li>
-          ))}
-        </ul>
-      </div>
       <div className={`
         h-full min-h-[calc(20rem-3rem)]
         flex flex-col justify-between
       `}>
-        <div>
-          <div className="overflow-x-auto rounded-lg border border-gray-300 bg-gradient-glass backdrop-blur-[25px]">
-            <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs  text-primary-800 uppercase bg-primary-500/5">
-                <tr>
-                  <th className="px-3 py-2 font-bold">Chamados {callStatus[filter.status]}</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {showingNotifications.slice(
-                  pageIndex * perPage,
-                  (pageIndex + 1) * perPage
-                ).map(notification => (
-                  <tr className="hover:bg-gray-200/80 border-b last:border-none" key={notification.id}>
-                    <td className="px-3 py-4 w-5">
-                      <input
-                        type="checkbox"
-                        id={`check-row-home-table-${notification.id}`}
-                        className={`check-row-home-table bg-transparent border-2`}
-                        data-name={notification.title}
-                        value={notification.id}
-                        onClick={(e) => {
-                          let hasAtLeast1Checked = document.querySelectorAll(`.check-row-home-table:checked`).length > 0;
-                          let checkAll = document.getElementById(`check-row-home-table`) as HTMLInputElement;
-
-                          if(hasAtLeast1Checked){
-                            setHasSelected(true);
-
-                            checkAll.checked = document.querySelectorAll(
-                              `.check-row-home-table:not(:checked)`
-                            ).length === 0;
-                          }
-                          else{
-                            setHasSelected(false);
-                            checkAll.checked = false;
-                          }
-                        }}
-                      />
-                    </td>                            
-
-                    <td
-                      className="px-3 py-4 cursor-pointer"
-                      onClick={() => {}}
-                    >
-
-                      <div className="flex items-center gap-2 max-w-full">
-                        <strong className="text-sm max-w-[calc(100%-2rem)] truncate">{notification.title}</strong>
-                      </div>
-                      <div className="flex flex-col gap-2 mt-0.5">
-                        <span className="text-gray-500 text-xs font-normal">
-                          {notification.description.slice(0, 80) + (notification.description.length > 80 ? '...':'')}
-                        </span>
-                        <span className="text-gray-400 text-xs">{moment(notification.schedule).format('DD/MM/YYYY H:mm')}</span>
-                      </div>
-                    </td>
-                    <td className="px-3 py-4 cursor-pointer text-end">
-                      <div className="flex items-center gap-2 justify-end">
-                        <button
-                          type="button"
-                          className="
-                            rounded-lg border leading-none
-                            shadow-sm hover:bg-gray-200
-                            text-gray-500 font-semibold
-                            focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2
-                          "
-                          onClick={() => {}}
-                        >
-                          {notification.viewed ? (
-                            <EnvelopeIcon w={20} h={20}/>
-                          ):(
-                            <EnvelopeOpenIcon w={20} h={20}/>
-                          )}
-                        </button>
-                        <button
-                          type="button"
-                          className="
-                            rounded-lg border leading-none
-                            shadow-sm hover:bg-gray-200
-                            text-gray-500 font-semibold
-                            focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2
-                          "
-                          onClick={() => {}}
-                        >
-                          {notification.is_archived ? (
-                            <DropboxIcon w={20} h={20}/>
-                          ):(
-                            <ArchiveIcon w={20} h={20}/>
-                          )}
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-                {showingNotifications.length === 0 && (
-                  <tr>
-                    <td
-                      colSpan={3}
-                      className="px-3 py-10 text-center"
-                    >
-                      {isFiltering ? (
-                        'Não há notificações com esse filtro'
-                      ):(
-                        filter.status === 'open' ? 'Todas as notificações já foram visualizadas' :
-                        filter.status === 'in_progress' ? 'Não há notificações visualizadas' :
-                        filter.status === 'finished' ? 'Não há notificações arquivadas':'...'
-                      )}
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
+        {calleds.map(called => (
+          <div className="hover:bg-gray-200/80 border-b last:border-none" key={called._id}>
+            <div className="px-3 py-4 cursor-pointer">
+              <div className="flex items-center gap-2 max-w-full">
+                <strong className="text-sm max-w-[calc(100%-2rem)] truncate">{'Em Aberto'}</strong>
+              </div>
+              <div className="flex flex-col gap-2 mt-0.5">
+                <span className="text-gray-500 text-xs font-normal">
+                  {called.data.description.slice(0, 80) + (called.data.description.length > 80 ? '...':'')}
+                </span>
+                <span className="text-gray-400 text-xs">{moment(called.updated_at).format('DD/MM/YYYY H:mm')}</span>
+              </div>
+            </div>
+            <div className="px-3 py-4 cursor-pointer text-end">
+              <div className="flex items-center gap-2 justify-end">
+                <button
+                  type="button"
+                  className="
+                    rounded-lg border leading-none
+                    shadow-sm hover:bg-gray-200
+                    text-gray-500 font-semibold
+                    focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2
+                  "
+                  onClick={() => handleAccessCalled(called._id)}
+                ><ChevronDownIcon className="-rotate-90"/></button>
+              </div>
+            </div>
           </div>
-        </div>
-        <TableFooter
-          perPage={perPage}
-          pageIndex={pageIndex}
-          totalPages={isFiltering ? Math.ceil(showingNotifications.length / perPage) : totalPages}
-          total={isFiltering ? showingNotifications.length : total[filter.status] ?? 0}
-
-          canPreviousPage={canPreviousPage}
-          canNextPage={canNextPage}
-
-          goToPage={() => {}}
-          previousPage={() => {}}
-          nextPage={() => {}}
-
-          isFiltering={isFiltering}
-        />
-        {isLoading && (
-          <div className="absolute inset-0 z-50 bg-gray-50/75">
-            <Spinner
-              aria-label="Carregando"
-            />
+        ))}
+        {calleds.length === 0 && (
+          <div className="px-3 py-16 text-center bg-gray-100 rounded-lg text-gray-500 text-sm">
+            Não há nenhum chamado em aberto
           </div>
-        )}
+        )}  
       </div>
+      {isLoading && (
+        <div className="absolute inset-0 z-50 bg-gray-50/75 flex items-center justify-center text-center">
+          <Spinner
+            aria-label="Carregando"
+          />
+        </div>
+      )}
     </div>
   )
 }
