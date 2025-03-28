b74eac69c33d579acfa8c0831af8deac9ad7be96
Franciel Santos Silva <francielrpm@gmail.com>
2025-03-28 13:48:37 +0000

    adicionado conversations na tipagem do formulario

--- Wrapper/v3/ButtonHelp.tsx
+++ Wrapper/v3/ButtonHelp.tsx
@@ -35,6 +35,13 @@ export const translateCallStatus : Record<CallStatusType, string> = {
   in_progress: 'Em Andamento',
   finished: 'Finalizados',
 }
+export interface ChatMessage {
+  message: string;
+  date: string;
+  username: string;
+  is_support: boolean;
+  id: string;  
+}
 export interface CalledType{
   _id: string,
   _user_id?: string,
@@ -49,6 +56,7 @@ export interface CalledType{
   attachments?: { id: string, name: string, url: string }[],
   internal_review_description?: string,
   created_call_url: string,
+  conversations?: ChatMessage[]
 }
 
 interface FileListType{
