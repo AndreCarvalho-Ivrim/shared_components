f2056aed16551f5904ebd9f2942aaf7a865f2245
Franciel Santos Silva <francielrpm@gmail.com>
2025-04-08 11:06:54 +0000

    feat: adicionado a opção de omit do aside no Wrapper

--- Wrapper/v3/Wrapper.tsx
+++ Wrapper/v3/Wrapper.tsx
@@ -23,20 +23,25 @@ interface WrapperProps{
 }
 export const Wrapper = ({ breadcrumbs, children, asideItems, dynamicAsideItems, footerItems, asideActive, module_name, omit, goBack }: WrapperProps) => (
   <div className="w-screen h-screen bg-background sm:p-4 !pr-0">
-    <div className="
+    <div className={`
       grid max-w-[1496px] min-[1700px]:max-w-[85%] mx-auto h-full 
-      sm:grid-cols-aside-collapsed md:grid-cols-aside-expanded-md 
-      lg:grid-cols-aside-expanded
-      group-[.collapsed-desktop-aside]:sm:grid-cols-aside-collapsed
-    ">
-      <Aside
-        items={asideItems}
-        dynamicItems={dynamicAsideItems}
-        active={asideActive}
-        module_name={module_name}
-        footerItems={footerItems}
-        goBack={goBack}
-      />
+      ${omit.includes('aside') 
+        ? 'grid-cols-1'
+        : `sm:grid-cols-aside-collapsed md:grid-cols-aside-expanded-md 
+          lg:grid-cols-aside-expanded
+          group-[.collapsed-desktop-aside]:sm:grid-cols-aside-collapsed`
+      }
+    `}>
+      {!omit.includes('aside') && (
+        <Aside
+          items={asideItems}
+          dynamicItems={dynamicAsideItems}
+          active={asideActive}
+          module_name={module_name}
+          footerItems={footerItems}
+          goBack={goBack}
+        />
+      )}
       <div className="pr-8 py-8 pl-8 sm:pl-12 max-h-screen -my-4 overflow-y-auto" id="wrapper-content">
         {!omit.includes('header') && (
           <Header breadcrumbs={breadcrumbs}/>
