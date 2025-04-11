48ad9373ca77af39b97d63661d81b91395305fcc
Mateus Brandão <mateusfleria@gmail.com>
2025-04-11 08:58:13 -0300

    fix: Removendo duplicidade de prop no merge

--- Wrapper/v3/ButtonHelp.tsx
+++ Wrapper/v3/ButtonHelp.tsx
@@ -53,7 +53,6 @@ export interface CalledType{
   updated_at: string,
   user: { id: string, name: string, email: string },
   client: { id: string, name: string },
-  type_of_call: 'Melhoria' | 'Dúvida' | 'Problema',
   description: string,
   type_of_call: TypeOfCallType,
   attachments?: { id: string, name: string, url: string }[],
