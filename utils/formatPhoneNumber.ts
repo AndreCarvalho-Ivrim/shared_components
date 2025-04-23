3e709c6b02ffad9a26a652c7c0cfbf41e9632979
Lucas Santos <santosmalachias@gmail.com>
2025-04-23 17:11:56 -0300

    feat: inclusão da função getContactStatus no utils

--- utils/formatPhoneNumber.ts
+++
@@ -1,6 +0,0 @@
-export const formatPhoneNumber = (phone: string) => {
-    if (phone && phone.length === 13 && phone.startsWith("55")) {
-      return `(${phone.substring(2, 4)}) ${phone.substring(4, 9)}-${phone.substring(9)}`;
-    }
-    return phone;
-  };
